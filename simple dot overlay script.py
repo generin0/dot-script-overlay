78d628ec48717f18d1edf6125b37525756287a24
Generin0 <dimas1234ssa@gmail.com>
2025-04-02 20:01:48 +0300

    simple dot overlay script.py

--- simple dot overlay script.py
+++ simple dot overlay script.py
@@ -3,7 +3,7 @@ from pynput.keyboard import Key, Listener
 import threading
 import random
 
-root = tk.Tk()
+tinker = tk.Tk()
 is_visible = True
 current_color = 'red'
 
@@ -16,16 +16,16 @@ def on_press(key):
     global is_visible
 
     if key == Key.ctrl_l or key == Key.ctrl_r:
-        root.after(0, change_color)
+        tinker.after(0, change_color)
 
     if key == Key.space:
-        root.destroy()
+        tinker.destroy()
 
     if key == Key.shift:
         if is_visible:
-            root.withdraw()
+            tinker.withdraw()
         else:
-            root.deiconify()
+            tinker.deiconify()
         is_visible = not is_visible
 
 def keyboard_listener():
@@ -35,25 +35,25 @@ def keyboard_listener():
 def main():
     global canvas, dot
 
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
+    screen_width = tinker.winfo_screenwidth()
+    screen_height = tinker.winfo_screenheight()
 
     print(screen_width,"x", screen_height)
     print("Press (Ctrl) to change the color.")
     print("Press (Space) to exit the overlay.")
     print("Press (Shift) to show or hide overlay.")
 
-    root.overrideredirect(True)
-    root.attributes("-topmost", True)
-    root.geometry(f'20x20+{screen_width//2-10}+{screen_height//2-10}')
-    root.resizable(width=False, height=False)
-    root.wm_attributes("-transparentcolor", "black")
+    tinker.overrideredirect(True)
+    tinker.attributes("-topmost", True)
+    tinker.geometry(f'20x20+{screen_width//2-10}+{screen_height//2-10}')
+    tinker.resizable(width=False, height=False)
+    tinker.wm_attributes("-transparentcolor", "black")
 
-    canvas = tk.Canvas(root, width=20, height=20, bg='black', highlightthickness=0)
+    canvas = tk.Canvas(tinker, width=20, height=20, bg='black', highlightthickness=0)
     canvas.pack()
     dot = canvas.create_oval(5, 5, 8, 8, fill=current_color, outline=current_color)
 
-    root.mainloop()
+    tinker.mainloop()
 
 listener_thread = threading.Thread(target=keyboard_listener, daemon=True)
 listener_thread.start()
